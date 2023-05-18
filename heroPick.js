85edf92be832af28640ab3cd04f1970100a95bc5
Zasada94 <pawelzasada2@wp.pl>
2023-05-18 15:14:34 +0200

    hero pick page finished

--- heroPick.js
+++ heroPick.js
@@ -177,3 +177,22 @@ searchInput2.addEventListener("keyup", (e) => {
 		searchButton2.click();
 	}
 });
+
+
+//responsive fonts
+const body = document.getElementById("body");
+const wrapper = document.getElementById("wrapper");
+
+function responsiveFonts() {
+	windowWidth = body.offsetWidth;
+	pageWidth = wrapper.offsetWidth;
+	if (pageWidth >= windowWidth) {
+		wrapper.classList.add("active");
+	} else {
+		wrapper.classList.remove("active");
+	}
+}
+responsiveFonts();
+window.addEventListener("resize", () => {
+	responsiveFonts();
+});
